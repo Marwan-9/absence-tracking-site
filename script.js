0b5ddad81a03f3d514c1ae17e66c7b91dc12bf0d
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2022-10-26 23:35:03 +0200

    update

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const agreeButton=document.getElementsByClassName("acceptButton");
 //==== Global Variables
 let coursesArray = [];
 let divHeightArray = [10,13,16,19,22,25,28,31];
-let divHeightArrayMobile = [15,20,23,27,30,35,40,43];
+let divHeightArrayMobile = [10,10,12,13.5,15.5,17.75,19.75,21.75];
 let securityKey=0;
 let outputsNumber;
 let firstWarning;
@@ -434,7 +434,7 @@ keyFrames.innerHTML = `
 
 @media only screen and (max-width: 1024px) {
   @keyframes verticalwipe{ 
-    from { height: 0;} to {height:` + heightMobile + `vh;}
+    from { height: 0;} to {height:` + heightMobile + `rem;}
   }  
 }
 `;
