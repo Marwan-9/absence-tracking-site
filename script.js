0a0ffb05149fe5b0b23b29ef36688c4ab67f9c7e
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2022-11-05 00:03:48 +0200

    commit 7

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const agreeButton=document.getElementsByClassName("acceptButton");
 //==== Global Variables
 let coursesArray = [];
 let divHeightArray = [12,13,16,19,22,25,28,31];
-let divHeightArrayMobile = [40,40,50,55,65,72,81,90];
+let divHeightArrayMobile = [37,40,47,55,64,71,80,87];
 let OddGENsCourse = ["GEN341","GEN342","GEN004","GEN333"];
 let securityKey=0;
 let outputsNumber;
