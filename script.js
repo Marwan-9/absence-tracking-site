05177729d446a4cf64bf6787e30614fa8a5082b7
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2022-11-06 01:34:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -88,6 +88,13 @@ pics[0].addEventListener("click",linkedIn1);
 pics[1].addEventListener("click",linkedIn2);
 
 
+window.addEventListener('load', (event) => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TRY01RG3HX');
+});
 
 
 //==== Main Functions:
